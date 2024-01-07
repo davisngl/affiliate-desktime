e37a5b1ffc0e4c034d757a9d0a3fe055e22d7d12
Dāvis Naglis <davisnaglis2@gmail.com>
2024-01-07 17:49:30 +0200

    Add affiliate code owner and user relationships

--- app/Models/AffiliateCode.php
+++ app/Models/AffiliateCode.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AffiliateCode extends Model
 {
     use HasFactory;
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -43,6 +45,21 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'password'          => 'hashed',
     ];
+
+    public function affiliateCodes(): HasMany
+    {
+        return $this->hasMany(AffiliateCode::class);
+    }
+
+    public function referrer(): HasOneThrough
+    {
+        return $this->hasOneThrough(
+            User::class,
+            AffiliateCode::class,
+            'user_id',
+            'referrer_id'
+        );
+    }
 }
